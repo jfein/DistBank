7f3fe90ddf9db27dd2a2108945cf2672b78999b6
vera <vsk23@cornell.edu>
2012-09-10 18:50:57 -0400

    project changes
    added objects

---
+++ workspace/DistBank/src/frontend/bank/objects/Branch.java
@@ -0,0 +1,74 @@
+package frontend.bank.objects;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Branch {
+	
+     private Integer branchId;
+     private HashMap<Integer,Account> branchAccounts;
+    
+     public Branch(Integer branchId) {
+    	 this.branchId = branchId;
+    	 this.branchAccounts = new HashMap<Integer, Account>();
+     }
+     
+     private void addAccount(Account account) {
+    	 this.branchAccounts.put(account.getAccountNumber(), account);
+     }
+     
+     private int Deposit(Integer accountId, Double amount, Integer serialNumber) {
+    	 //check if this accountId exists
+    	 if (branchAccounts.containsKey(accountId)) {
+    		 return -1;
+    	 }
+    	 //TODO: check the serial number
+    	 // Set account amount
+    	 Account accountToDo = branchAccounts.get(accountId);
+    	 accountToDo.setAccountBalance(accountToDo.getAccountBalance() + amount);
+    	 branchAccounts.put(accountId, accountToDo);
+    	 return 1; 
+     }
+     
+     private int Withdraw(Integer accountId, Double amount, Integer serialNumber) {
+    	 //check if this accountId exists
+    	 if (branchAccounts.containsKey(accountId)) {
+    		 return -1;
+    	 }
+    	 //TODO: check the serial number
+    	 // Set account amount
+    	 Account accountToDo = branchAccounts.get(accountId);
+    	 accountToDo.setAccountBalance(accountToDo.getAccountBalance() - amount);
+    	 branchAccounts.put(accountId, accountToDo);
+    	 return 1; 
+     }
+     
+     private Double Query(Integer accountId, Integer serialNumber) {
+    	 //check if this accountId exists
+    	 if (branchAccounts.containsKey(accountId)) {
+    		 return -1.0;
+    	 }
+    	 //TODO: check the serial number
+    	 // Set account amount
+    	 Account accountToDo = branchAccounts.get(accountId);
+    	 return accountToDo.getAccountBalance();
+     }
+     
+     private int Transfer(Integer srcAccountId, Integer destAccountId, Double amount, Integer serialNumber) {
+    	 //check if this accountId exists
+    	 if (branchAccounts.containsKey(srcAccountId)) {
+    		 return -1;
+    	 }
+    	
+    	 //TODO: check the serial number
+    	 
+    	 // Update the source account amount
+    	 Account accountToDo = branchAccounts.get(srcAccountId);
+    	 accountToDo.setAccountBalance(accountToDo.getAccountBalance() - amount);
+    	 branchAccounts.put(srcAccountId, accountToDo);
+    	 
+    	 //TODO: Send a deposit request to the destination
+    	 return 1; 
+     }
+
+}
