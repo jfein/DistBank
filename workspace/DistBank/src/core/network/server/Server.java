f8e4ceaa1148320dbffaca9c319de189cb177e37
Jeremy Fein <jeremy.d.fein@gmail.com>
2012-09-10 21:15:59 -0400

    close socket on server failure
    when something in the main server loop fails, will now close connection
    on server side. we should rethink the whole socket thing though, like
    who owns / is responsible for a socket. this is an easy fix now to get
    unidirectional stuff working. things just throw errors now in that case.

--- workspace/DistBank/src/core/network/server/Server.java
+++ workspace/DistBank/src/core/network/server/Server.java
@@ -22,17 +22,33 @@ public class Server extends Thread {
 	@Override
 	public void run() {
 		while (!serverSocket.isClosed()) {
+			Socket conn = null;
 			try {
-				Socket conn = serverSocket.accept();
+				conn = serverSocket.accept();
+
+				System.out.println("New Connection from "
+						+ conn.getRemoteSocketAddress());
 
 				Message msgIn = NetworkInterface.getMessage(conn);
 
+				System.out.println("Got Request");
+
 				Message msgOut = serverHandler.handle(msgIn);
+
+				System.out.println("Request Handled");
 
 				NetworkInterface.sendMessage(conn, msgIn.getReturnAddress(),
 						msgOut);
+
+				System.out.println("Sent Response");
 			} catch (Exception e) {
 				e.printStackTrace();
+				try {
+					conn.close();
+				} catch (IOException e2) {
+					e2.printStackTrace();
+				}
+
 			}
 		}
 	}
