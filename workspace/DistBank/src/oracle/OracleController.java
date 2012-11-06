c63de18212dff7b445b8591a76968696e4442e48
vera <vsk23@cornell.edu>
2012-11-06 16:23:18 -0500

    ORACLECONTROLLER

--- workspace/DistBank/src/oracle/OracleController.java
+++ workspace/DistBank/src/oracle/OracleController.java
@@ -2,6 +2,7 @@ package oracle;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 
 public class OracleController implements Runnable {
@@ -10,6 +11,8 @@ public class OracleController implements Runnable {
 	
 	private OracleView oracleView;
 	private OracleState oracleState;
+
+	private String recoverCommand = "/Users/verakutsenko/Documents/DistBank/workspace/DistBank/LAUNCH_MAC.shell ";
 	
 	@Override
 	public void run() {
@@ -31,7 +34,7 @@ public class OracleController implements Runnable {
 		public void actionPerformed(ActionEvent arg0) {
 			//Get input from oracle about node id to fail
 			//Generate a fail request
-			//send a DIE request to that node
+			//send a DIE request to that nodeId
 		
 			
 		}
@@ -43,8 +46,8 @@ public class OracleController implements Runnable {
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
 			//Get input from oracle about node if to notify about
-			// send a mess id to all apps that are interested in that node id
-			//change the state of the oracle and remove into a "fail list" (if we want to 
+			//Send a failure notification to Node Id, to make it remove the node off it's mapping from app to nodes
+			//change the state of the oracle and remove into a "fail list" (if we want to implement that)
 		}
 		
 	}
@@ -52,15 +55,28 @@ public class OracleController implements Runnable {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-			
+			//recover the node, send command line to start up the node
+			Runtime rt = Runtime.getRuntime();
+			try {
+		    	Process pr = rt.exec("/Users/verakutsenko/Documents/DistBank/workspace/DistBank/LAUNCH_MAC.shell");
+		        int exitVal = pr.waitFor();
+		        System.out.println("Exited with error code "+exitVal);
+		        
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
 		}
+	}
 	class NotifyRecoverListener implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
+			//send a message to NODE ID, to tell it to add the nodeid to those who
+			//were previously interested
 			
 		}
 		
